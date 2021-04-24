22b71944c8180f93e96ee8cb765cd36881a790dd
robertramosastudillo <robertramosastudillo@gmail.com>
2021-04-23 20:16:24 -0500

    Url Index was modified to implemented for the deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: `/https://robertramosastudillo.github.io`,
+  pathPrefix: `robertramosastudillo.github.io`,
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
